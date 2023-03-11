454d81e621f968fdad0b91fbf5a69392f56f6f7a
Максим Расторгуев <frontimax@yandex.ru>
2023-03-11 15:01:20 +0400

    added contract component

---
+++ src/app/components/contarct/contarct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contarct',
+  templateUrl: './contarct.component.html',
+  styleUrls: ['./contarct.component.scss']
+})
+export class ContarctComponent {
+
+}
