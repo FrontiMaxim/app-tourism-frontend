7d24f04569d0d0967fce0941533968f774d4326e
Максим Расторгуев <frontimax@yandex.ru>
2023-03-11 15:04:45 +0400

    started designing routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContarctComponent } from './components/contarct/contarct.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContarctComponent},
+  { path: 'contracts', component: ContarctComponent},
+  { path: 'clients', component: ContarctComponent},
+  { path: 'hotels', component: ContarctComponent},
+  { path: 'permits', component: ContarctComponent},
+  { path: 'discounts', component: ContarctComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
