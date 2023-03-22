caba48e38cb0d5efb94737b11cfe9077424c9f40
Максим Расторгуев <frontimax@yandex.ru>
2023-03-22 16:11:39 +0400

    started work by contract

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContarctComponent } from './components/contarct/contarct.component';
 import { ClientPageComponent } from './client/pages/client-page/client-page.component';
 import { PageHotelComponent } from './hotel/pages/page-hotel/page-hotel.component';
 import { PagePermitComponent } from './permit/pages/page-permit/page-permit.component';
 import { PageDiscountComponent } from './discount/pages/page-discount/page-discount.component';
+import { PageContractComponent } from './contract/pages/page-contract/page-contract.component';
 
 const routes: Routes = [
-  { path: '', component: ContarctComponent},
+  { path: '', component: PageContractComponent},
   { path: 'clients', component: ClientPageComponent},
   { path: 'hotels', component: PageHotelComponent},
   { path: 'permits', component: PagePermitComponent},
